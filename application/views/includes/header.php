c5c9cc4d1637393a7d091deedca9924edb0b078b
Lochanahasith <lochanahm@gmail.com>
2018-04-05 00:17:02 +0530

    Admin panel added

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -11,39 +11,39 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!-- for-mobile-apps -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Smart Shop Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+<meta name="keywords" content="Smart Shop Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
 		function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!-- //for-mobile-apps -->
-<link href="<?php echo base_url()?>template/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="<?php echo base_url() ?>template/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
 <!-- pignose css -->
-<link href="<?php echo base_url()?>template/css/pignose.layerslider.css" rel="stylesheet" type="text/css" media="all" />
+<link href="<?php echo base_url() ?>template/css/pignose.layerslider.css" rel="stylesheet" type="text/css" media="all" />
 
 
 <!-- //pignose css -->
-<link href="<?php echo base_url()?>template/css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="<?php echo base_url() ?>template/css/style.css" rel="stylesheet" type="text/css" media="all" />
 <!-- js -->
-<script type="text/javascript" src="<?php echo base_url()?>template/js/jquery-2.1.4.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>template/js/jquery-2.1.4.min.js"></script>
 <!-- //js -->
 <!-- cart -->
-	<script src="<?php echo base_url()?>template/js/simpleCart.min.js"></script>
+	<script src="<?php echo base_url() ?>template/js/simpleCart.min.js"></script>
 <!-- cart -->
 <!-- for bootstrap working -->
-	<script type="text/javascript" src="<?php echo base_url()?>template/js/bootstrap-3.1.1.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url() ?>template/js/bootstrap-3.1.1.min.js"></script>
 <!-- //for bootstrap working -->
 <link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 <link href='//fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,900,900italic,700italic' rel='stylesheet' type='text/css'>
 
 
 
-<script src="<?php echo base_url()?>template/js/jquery.easing.min.js"></script>
+<script src="<?php echo base_url() ?>template/js/jquery.easing.min.js"></script>
 
 <style type="text/css">
-.pseudolink { 
-   color:blue; 
-   text-decoration:underline; 
-   cursor:pointer; 
+.pseudolink {
+   color:blue;
+   text-decoration:underline;
+   cursor:pointer;
    }
 </style>
 
@@ -65,7 +65,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <div class="header-bot">
 	<div class="container">
 		<div class="col-md-3 header-left">
-			<h1><a href="index.html"><img src="<?php echo base_url()?>template/images/logo3.jpg"></a></h1>
+			<h1><a href="index.html"><img src="<?php echo base_url() ?>template/images/logo3.jpg"></a></h1>
 		</div>
 		<div class="col-md-6 header-middle">
 			<form>
@@ -75,7 +75,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				<div class="section_room">
 					<select id="country" onchange="change_country(this.value)" class="frm-field required">
 						<option value="null">All categories</option>
-						<option value="null">Electronics</option>     
+						<option value="null">Electronics</option>
 						<option value="AX">kids Wear</option>
 						<option value="AX">Men's Wear</option>
 						<option value="AX">Women's Wear</option>
@@ -89,15 +89,15 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			</form>
 		</div>
 
-		<?php 
-	 $loggerFname = $this->session->userData('loggerFname');
-	 $loggerLname = $this->session->userData('loggerLname');
-	  if( $loggerFname == ""){
-	 ?>
+		<?php
+$loggerFname = $this->session->userData('loggerFname');
+$loggerLname = $this->session->userData('loggerLname');
+if ($loggerFname == "") {
+	?>
 		<div class="col-md-3 header-right footer-bottom">
 			<ul>
 				<li><a href="#" class="use1" data-toggle="modal" data-target="#myModal4"><span style="width:180px;">Login</span></a>
-					
+
 				</li>
 				<li><a class="fb" href="#"></a></li>
 				<li><a class="twi" href="#"></a></li>
@@ -106,18 +106,17 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			</ul>
 		</div>
 
-		<?php 
-	}else{
+		<?php
+} else {
 
-
-		?>
+	?>
 
 
 		<div class="col-md-3 header-right footer-bottom">
 			<ul>
 				<li><a href="#" class="use1">
-			<span class="pseudolink" onclick="location='<?php echo base_url()?>index.php/User/signout'" style="width:180px;"><?php echo $loggerFname." ".$loggerLname." "."[ Logout ]" ; ?></span></a>
-					
+			<span class="pseudolink" onclick="location='<?php echo base_url() ?>index.php/User/signout'" style="width:180px;"><?php echo $loggerFname . " " . $loggerLname . " " . "[ Logout ]"; ?></span></a>
+
 				</li>
 				<li><a class="fb" href="#"></a></li>
 				<li><a class="twi" href="#"></a></li>
@@ -126,11 +125,11 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			</ul>
 		</div>
 
-		<?php 
+		<?php
 
-	}
+}
 
-	?>
+?>
 
 
 		<div class="clearfix"></div>
