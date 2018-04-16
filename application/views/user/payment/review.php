c5728f6a7a7bbcd9fa3229d8830a97d37916c12c
Unknown <icbjayasinghe@gmail.com>
2018-04-16 12:04:40 +0530

    review page form editing

--- application/views/user/payment/review.php
+++ application/views/user/payment/review.php
@@ -19,4 +19,30 @@
 <!--            <li class="visited"><em>Review</em></li>-->
         </ol>
     </nav>
+
+    <div>
+        <form>
+            <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                <div class="col-sm-10">
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <div class="col-sm-10">
+                    <button type="submit" class="btn btn-primary">Save and ship to this address</button>
+                </div>
+            </div>
+        </form>
+    </div>
+
 </section>
+
+
