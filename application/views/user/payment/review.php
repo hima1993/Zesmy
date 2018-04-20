5e29646f8352f50fc079c5f42142df838d300d0a
Unknown <icbjayasinghe@gmail.com>
2018-04-20 11:34:11 +0530

    Review page field content

--- application/views/user/payment/review.php
+++ application/views/user/payment/review.php
@@ -23,17 +23,51 @@
     <div>
         <form>
             <div class="form-group row">
-                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-                <div class="col-sm-10">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Contact Name</label>
+                <div class="col-sm-6">
                     <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
                 </div>
             </div>
             <div class="form-group row">
-                <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Country/Region</label>
                 <div class="col-sm-10">
-                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                    <input type="email" class="form-control" id="inputPassword3" placeholder="Country/Region">
                 </div>
             </div>
+            <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Street Address</label>
+                <div class="col-sm-6">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Street">
+                </div>
+                <div class="col-sm-6">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Apartment, suite, unit etc (Optional)">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">State/Province/Region</label>
+                <div class="col-sm-6">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="State/Province/Region">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">City</label>
+                <div class="col-sm-6">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="City">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Zip/Postal Code</label>
+                <div class="col-sm-6">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Zip/Postal Code">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Mobile</label>
+                <div class="col-sm-6">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Zip/Postal Code">
+                </div>
+            </div>
+
 
             <div class="form-group row">
                 <div class="col-sm-10">
